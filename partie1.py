964087fa3c5ca4f674e2e1b5f9aa8df91a2a99e9
Marie Caron <marie.caron@epitech.eu>
2025-09-09 17:10:24 +0200

    feat: creation read_parquet_batch function.

--- partie1.py
+++ partie1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from fastparquet import ParquetFile
+import pyarrow.parquet as pq
 
 
 def read_parquet(filename) :
@@ -12,5 +13,17 @@ def read_parquet_columns(filename, columns) :
     specific_selection = file_read.iloc[columns]
     print(specific_selection)
 
+def read_parquet_batch(filename, batch_size) :
+
+    file_parquet = pd.read_parquet(filename)
+    parquet_file = pq.ParquetFile(filename)
+
+    for i in parquet_file.iter_batches(batch_size) :
+        print("RecordBatch")
+        file_parquet = i.to_pandas()
+        break
+        
+
 #read_parquet("./flights.parquet")
 #read_parquet_columns("./flights.parquet", 2)
+#read_parquet_batch("./flights.parquet", 350)
