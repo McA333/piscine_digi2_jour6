54cfc7ebefc21b0b0977ca190488d9200cbec51c
Marie Caron <marie.caron@epitech.eu>
2025-09-09 16:56:59 +0200

    feat: creation read_parquet_columns function.

--- partie1.py
+++ partie1.py
@@ -1,8 +1,16 @@
 import pandas as pd
+from fastparquet import ParquetFile
+
 
 def read_parquet(filename) :
 
     file_parquet = pd.read_parquet(filename)
     print(file_parquet.head(10))
 
-read_parquet("./flights.parquet")
+def read_parquet_columns(filename, columns) :
+    file_read = pd.read_parquet(filename)
+    specific_selection = file_read.iloc[columns]
+    print(specific_selection)
+
+#read_parquet("./flights.parquet")
+#read_parquet_columns("./flights.parquet", 2)
